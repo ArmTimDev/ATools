05482f9287b82f437c0ea687293dd71b6498affa
ArmTimDev <76068426+ArmTimDev@users.noreply.github.com>
2020-12-29 18:28:47 +0330

    Create Date Viewer.sh
    Great !! Just See This !!

---
+++ Source Codes/Date Viewer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ 
+CURRENTDATE=`date +"%Y-%m-%d %T"`
+CURRENTDATEONLY=`date +"%b %d, %Y"`
+ 
+echo Current Date is: ${CURRENTDATEONLY}
+echo Current Date and Time is: `date +"%Y-%m-%d %T"`
+echo Current Date and Time is: ${CURRENTDATE}
