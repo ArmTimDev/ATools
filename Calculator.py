683291d74717f784debda7330cfd094f1bc81f4f
ArmTimDev <76068426+ArmTimDev@users.noreply.github.com>
2020-12-29 17:37:11 +0330

    Delete Calculator.py

--- Calculator.py
+++
@@ -1,5 +0,0 @@
-def Calculate():
-  A = int(input("Hello ! Please Enter Your First Number You Want To Calculate : "))
-  B = int(input("Great ! Please Enter Your Second Number You Want To Calculate : "))
-  C = input("Now Choose An Action !")
-  
