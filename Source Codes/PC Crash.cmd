af3cf446ad6e0aecbe5151e3facf42117871c2ec
ArmTimDev <76068426+ArmTimDev@users.noreply.github.com>
2020-12-30 09:33:18 +0330

    Create PC Crash.cmd
    Guys It Will Crash Your PC !!!!
    
    ⚠ Warning : Do NOT Run This Program If You Have Less Than 4 Gigabyte Ram !! ⚠
    Be Careful 😅
    Use It If You Want Crash Your Friend PC 😂

---
+++ Source Codes/PC Crash.cmd
@@ -0,0 +1,22 @@
+cd Desktop
+mkdir Hi
+mkdir Hello
+mkdir How
+mkdir Are
+mkdir You
+mkdir Haha
+mkdir Bla Bla
+mkdir GitHub
+mkdir Hello
+mkdir Hi
+mkdir Hello
+mkdir How
+mkdir Are
+mkdir You
+mkdir Haha
+mkdir Bla Bla
+mkdir GitHub
+mkdir Hello
+tree
+cd Hi
+shutdown /p
