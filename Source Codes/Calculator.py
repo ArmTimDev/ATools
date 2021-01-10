18082fd84c74f7823035b98b6192f16943418204
ArmTimDev <76068426+ArmTimDev@users.noreply.github.com>
2021-01-10 12:34:13 +0330

    Update Calculator.py
    Ok Great

--- Source Codes/Calculator.py
+++ Source Codes/Calculator.py
@@ -1,4 +1,12 @@
 def Calculate():
   A = int(input("Hello ! Please Enter Your First Number You Want To Calculate : "))
   B = int(input("Great ! Please Enter Your Second Number You Want To Calculate : "))
-  C = input("Now Choose An Action !")
+  C = input("Now Choose An Action !\n\n1. Sum\n2. Minus")
+  if C == 'Sum':
+    print(A+B)
+  elif C == 'Minus':
+    print(A-B)
+  else:
+    Calculate()
+
+Calculate()
